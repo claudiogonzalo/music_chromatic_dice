361a5017385f098638fa6a314eadcf23817f2ea5
claudio gonzalo <gonzaloclaudio@gmail.com>
2021-08-20 21:07:56 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,17 @@
-input.onLogoEvent(TouchButtonEvent.Released, function () {
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.showNumber(randint(1, 7))
-    azar = randint(1, 5)
+    azar = randint(1, 8)
     for (let index = 0; index < azar; index++) {
         music.playTone(randint(131, 988), music.beat(BeatFraction.Eighth))
         music.rest(music.beat(BeatFraction.Eighth))
+        music.setTempo(randint(110, 130))
         music.playTone(randint(131, 988), music.beat(BeatFraction.Eighth))
         music.rest(music.beat(BeatFraction.Eighth))
+        music.setTempo(randint(110, 130))
         music.playTone(randint(131, 988), music.beat(BeatFraction.Eighth))
+        music.rest(music.beat(BeatFraction.Eighth))
     }
+    basic.clearScreen()
 })
 let azar = 0
 basic.showLeds(`
